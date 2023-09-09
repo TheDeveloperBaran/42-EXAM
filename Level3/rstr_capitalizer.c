0564f59c8655fdb59f60daeffcf7a63d9b0a6b51
Baran Kılıç <96842857+TheDeveloperBaran@users.noreply.github.com>
2023-09-10 01:23:55 +0300

    Create rstr_capitalizer.c
    proven with displaying iterator value and the current pointer of the whole string to the end of the loop

---
+++ Level3/rstr_capitalizer.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <unistd.h>
+
+void    rstr_capitalizer(char *str)
+{
+    int i = 0;
+    if (str[i] >= 'a' && str[i] <= 'z' && str[i + 1] == ' ')
+    {
+        str[i] -= 32;
+    }
+    else if (str[i] >= 'A' && str[i] <= 'Z')
+    {
+        str[i] += 32;
+    }
+    write(1, &str[i], 1);
+    while (str[++i])
+    {
+        printf("i: %d\n", i);
+        printf("str[%d]: %c\n", i, str[i]);
+        if (str[i] >= 'A' && str[i] <= 'Z' && (str[i + 1] != ' ' && str[i + 1] != '\t' 
+        && str[i + 1] != '\0'))
+        {
+            str[i] += 32;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' 
+        || str[i + 1] == '\0'))
+        {
+            str[i] -= 32;
+        }
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac < 2)
+    {
+        write(1, "\n", 1);
+    }
+    else
+    {
+        int i = 1;
+        while (i < ac)
+        {
+            rstr_capitalizer(av[i]);
+            write(1, "\n", 1);
+            i++;
+        }
+    }
+}
